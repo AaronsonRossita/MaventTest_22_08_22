0101719e6802fc90b5c55bbd6b8572b305ca3d89
AaronsonRossita <aaronsonrossita@gmail.com>
2022-08-22 21:26:40 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
 
+        System.out.println(1);
+        System.out.println(2);
+
+    }
 
 }
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,38 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class FirstTest {
 
-    
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver","res\\chromedriver.exe");
+    }
+
+    @Test
+    public static void testOne(){
+        WebDriver driver = new ChromeDriver();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.close();
+    }
+
+    @Test
+    public static void testTwo(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.close();
+    }
 
 }
